b32ab1d4e8a7f610a762a1f665824640619596d0
reket1990 <reket1990@yahoo.com>
2024-04-29 22:24:13 -0700

    install firebase

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Follow this pattern to import other Firebase services
+// import { } from 'firebase/<service>';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD5lDqvlr9v7Xakdhb6faGznTDzD_bfwFA",
+  authDomain: "dnd-timer-96d5b.firebaseapp.com",
+  projectId: "dnd-timer-96d5b",
+  storageBucket: "dnd-timer-96d5b.appspot.com",
+  messagingSenderId: "256275649175",
+  appId: "1:256275649175:web:a305c3b4801c4828f1cfd2"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a list of cities from your database
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
 
 function App() {
+  useEffect(() => {
+    const fetchData = async ()=> {
+      const data = await getCities(db);
+      return data;
+    }
+    const cities = fetchData();
+    console.log(cities);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
