55617bed46517265286debb63ff508f3241bf9b2
reket1990 <reket1990@yahoo.com>
2024-04-29 22:41:57 -0700

    remove tests

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
 
 const firebaseConfig = {
   apiKey: "AIzaSyD5lDqvlr9v7Xakdhb6faGznTDzD_bfwFA",
